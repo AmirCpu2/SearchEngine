b65414d38cfe3fd190ec730b4956d963e3b79032
Your Name <yourmail@mail.com>
2019-12-10 02:09:01 +0330

    Debug after Cleaning Code

--- Crowler/wiki_BLL.py
+++ Crowler/wiki_BLL.py
@@ -9,17 +9,23 @@ import re
 
 
 # values Init
-TokenList = []
-IndexList = []
-Command = ''
-Search = []
-Doc = ''
-db = []
+_limitLenghtShow   =  2
+_limitLenghtShow **=  2
+_words_print       = []
+checkList          = []
+Search             = []
+db                 = []
+dfTokenDoc         = ''
+dfToken            = ''
+dfIndex            = ''
+Command            = ''
+Doc                = ''
+State              =  0
 
 def CleanData (_command):
 
     # Command to String
-    Token = str(_command)
+    Token = str(_command).replace('\'','').replace('\"','')
 
     # Text Cleaning
     clean = bleach.clean(Token).replace('[ویرایش]','')
@@ -33,7 +39,7 @@ def CleanData (_command):
     clean = normalizer.normalize(clean)
 
     # Return => clean text
-    return _command
+    return clean
 
 
 def TokenInit (_Text):
@@ -50,21 +56,34 @@ def TokenInit (_Text):
     # Creat Rest Token
     for v in finallyList:Token.append(stemmer.stem(v))
 
-    return finallyList
+    return Token
 
 def LoadDatabase():
-    # this.Doc
-    global Doc,TokenList,IndexList,db
-
-    # LoadDocClear
-    # DocFile = open("DocFile.txt", 'r',encoding='utf-8', newline='')
-    # Doc = DocFile.read()
-    # DocFile.close()
-
+    # this.value
+    global dfToken,dfIndex,dfTokenDoc
+    
     # LoadDatabase
-    db = pd.read_csv("PandaDB.csv")
-    TokenList = db["Token"].tolist()
-    IndexList = db["Index"].tolist()
+        # Token[Id,Token]
+    dfToken = pd.read_csv("PandaDBToken.csv")
+    
+        # TokenIndex[TokenId,Index]
+    dfIndex = pd.read_csv("PandaDBIndex.csv")
+
+        # TokenDoc[Id,Token]
+    dfTokenDoc = pd.read_csv("PandaDBTokenDoc.csv")
+
+
+def ShowSentence(_wordsIndex,_lenght):
+   
+    for _wordId in _wordsIndex:
+        a = ''
+        for i in range(_lenght+_limitLenghtShow):
+            wTemp = list(dfTokenDoc['Token'].where(dfTokenDoc['Id'] == int(_wordId+i-_limitLenghtShow//2)).dropna().tolist()) 
+            try:
+                a += wTemp[0] + " "
+            except:
+                a += ""
+        print(str(a))
 
 #--------------------- Main ---------------------
 
@@ -76,9 +95,10 @@ while Command != 'exit()':
     # Value Local
     Word = []
     Search = []
-    
+    _words_print = []
+
     # Impot
-    Command = input('Pls Enter Command(exit() For End App)>\t').strip()
+    Command = input('Please enter Command(exit() to end the program)>>>  ').strip()
 
     # Exit Check
     if Command == 'exit()': exit()
@@ -87,16 +107,18 @@ while Command != 'exit()':
     elif 'or' in Command.lower() :
         # Text To lower For Split
         Text = Command.lower()
-        
+        State = False
+
         # [One Word,Tow Word]
-        Word = Text.Split('or')
+        Word = Text.split('or')
 
     elif 'and' in Command.lower() : 
         # Text To lower For Split
         Text = Command.lower()
+        State = True
         
         # [One Word,Tow Word]
-        Word = Text.Split('and')
+        Word = Text.split('and')
     else:
         # Text To lower For Split
         Word = [Command.lower()]
@@ -105,9 +127,35 @@ while Command != 'exit()':
     for w in Word: Search.append( TokenInit( CleanData(w) ) )
 
     # :) does it exist !!?
-    checkList = []
-    for s in Search:
-        for st in s:
-            checkList.append( db['Index'].where(db['Token']==st).dropna() )
-    
-    print(checkList)
+    # Select by TokenId of Token Table
+    for sl in Search:
+        tmp = []
+        (tmp.append( dfToken['Id'].where(dfToken['Token']==s).dropna().tolist() ) for s in sl)
+        checkList.append(tmp)
+        
+    # If available Command => Check Index Number and creat Word
+    for vl in checkList :
+        tmp = []
+        # If Not Null :/
+        if vl and vl[0] != [] :
+            # Select IndexId of Marge Table
+            (tmp.append(set(dfIndex['Index'].where(dfIndex['TokenId']==int(v[0])).dropna().tolist())) for v in vl if v != [])
+            
+            #-----------Finde Words together--------------
+            _lenght = len(tmp)
+                # [x] =>x-index : for equals Words together index
+            tmp = list((set(map(lambda x : x-i , tmp[i])) for i in range(len(tmp))))
+                # {x} => filter equls index : for finde intersection 
+            while(len(tmp) > 1): tmp = [ tmp[x]&tmp[x+1] for x in range(len(tmp)-1)] ,tmp.append(_lenght)
+                # [x]finde => not False
+            if len(tmp[0]) > 0 : _words_print.append(tmp)
+
+    # Show Sentence
+    if not _words_print or State :#if 'and' in Command || _words Finde length == 0  =>
+        # Compare the length of the input command with the tokens found
+        if len(_words_print) != len(Search) and not('\"' in Command or '\'' in Command) :
+            print('No words found')
+            continue
+
+    for v in _words_print:# Finde And print, {_limitLenghtShow BeforeToken}+ {_word_Print} + {_limitLenghtShow AfterToken}
+        ShowSentence(v[0],v[1])
